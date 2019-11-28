d07150365556d98fa21c19bd91b9c301d03baf8f
Carlos Querejeta <carlos.querejeta@boeing.com>
2019-11-28 23:42:22 +0100

    fixed fully connected layer sizes to support BiGRU and BiGAN)

--- qmodels.py
+++ qmodels.py
@@ -6,6 +6,7 @@ from torch.autograd.variable import Variable
 class QGenerator(nn.Module):
     def __init__(self, z_dim, output_size, hidRNN=100, nlayers=1, bidirectional=False, cell_type='LSTM', dropout=0):
         super(QGenerator, self).__init__()
+        num_directions = 2 if bidirectional else 1
         self.hidR       = hidRNN
         self.z_dim      = z_dim
         self.output_size = output_size
@@ -18,7 +19,7 @@ class QGenerator(nn.Module):
             raise Exception('Cell Type {} not recognized building the Generator'.format(cell_type))
 
         # self.GRU1       = nn.GRU(self.hidC, self.hidR)
-        self.fc         = nn.Linear(self.hidR, self.output_size)
+        self.fc         = nn.Linear(self.hidR*num_directions, self.output_size)
         self.tanh       = nn.Tanh()
         # self.init_lstm_bias()
         # for name, param in self.named_parameters():
@@ -44,6 +45,7 @@ class QGenerator(nn.Module):
 class QDiscriminator(nn.Module):
     def __init__(self, nfeatures, hidRNN=100, nlayers=1, bidirectional=False, cell_type='LSTM',dropout=0):
         super(QDiscriminator, self).__init__()
+        num_directions = 2 if bidirectional else 1
         self.output_size= nfeatures
         self.nfeatures  = nfeatures
         self.hidR       = hidRNN
@@ -56,7 +58,7 @@ class QDiscriminator(nn.Module):
             raise Exception('Cell Type {} not recognized building the Discriminator'.format(cell_type))
 
         # self.GRU1       = nn.GRU(self.hidC, self.hidR)
-        self.fc         = nn.Linear(self.hidR, self.output_size)
+        self.fc         = nn.Linear(self.hidR*num_directions, self.output_size)
         self.sigmoid    = nn.Sigmoid()
         print('\n Created Discriminator Class:\n ' + str(self))
 
